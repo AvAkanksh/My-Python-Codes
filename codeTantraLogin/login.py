84f71cba3a627372dcb7749925940246c85b52e5
AvAkanksh <ee18b002@iittp.ac.in>
2021-10-08 07:27:47 +0530

    updating the files

--- codeTantraLogin/login.py
+++ codeTantraLogin/login.py
@@ -2,14 +2,18 @@ from selenium import webdriver
 import time
 driver_path = "/usr/local/share/chromedriver"
 brave_path = "/usr/bin/brave"
-
 option = webdriver.ChromeOptions()
 option.binary_location = brave_path
 # option.add_argument("--incognito") #OPTIONAL
 # option.add_argument("--headless") #OPTIONAL
 
 # Create new Instance of Chrome
-driver = webdriver.Chrome(executable_path=driver_path, options=option)
+# driver = webdriver.Chrome(executable_path=driver_path, options=option)
+driver_path = '/usr/local/share/geckodriver'
+firefox_path = "/usr/bin/firefox"
+option = webdriver.FirefoxOptions()
+option.binary_location = firefox_path
+driver = webdriver.Firefox(executable_path=driver_path, options=option)
 
 driver.get("https://iittp.codetantra.com/login.jsp")
 
