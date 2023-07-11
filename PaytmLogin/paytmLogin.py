fb7e64aec5e99064d839e9790cee4a460f90de45
AvAkanksh <adugani.vanjari@paytm.com>
2023-07-11 20:28:06 +0530

    updating files

--- PaytmLogin/paytmLogin.py
+++ PaytmLogin/paytmLogin.py
@@ -7,6 +7,9 @@ import os
 from datetime import datetime
 import pyautogui as py
 import json
+import cv2
+from PIL import ImageGrab
+
 
 options = webdriver.ChromeOptions()
 s=Service(ChromeDriverManager().install())
@@ -18,19 +21,46 @@ password = driver.find_element(by=By.ID,value="loginForm:password")
 data = json.load(open("/Users/adugani.vanjari/Documents/GitRepositories/My-Python-Codes/PaytmLogin/credentials.json"))
 username.send_keys(data['username'])
 password.send_keys(data['password'])
-clicks=[[[590,370],[600,425],[770,325]],[[580,480],[580,530],[770,450]],[[580,520],[580,570],[770,490]]]
-i = 2
 print("Logging IN........")
 submit = driver.find_element(by=By.XPATH,value='//*[@id="loginForm:loginButton"]/span')
 driver.execute_script('arguments[0].click();',submit)
-time.sleep(35)
+time.sleep(20)
+
+# currentPosition = py.position()
+# position= (currentPosition[0]*2,currentPosition[1]*2)
+# print(position)
+
 print("Logged IN........")
-py.click(clicks[i][0][0],clicks[i][0][1])
-time.sleep(0.5)
-py.click(clicks[i][1][0],clicks[i][1][1])
-print("Selected Option Out of Office")
-# Set the location of the punch out according to your screen!
-time.sleep(0.5)
-py.click(clicks[i][2][0],clicks[i][2][1])
+
+# checking if color is blue  : (  8, 24, 64,255) (NEW UI) {oneWeb}
+# checking if color is white : (240,241,242,255) (OLD UI)
+
+image = ImageGrab.grab()
+position = (60,1600)
+color = image.getpixel(position)
+flag = True
+
+if(color[0]==240): 
+    print("Old UI")
+    time.sleep(0.5)
+    py.click(690,425)
+    time.sleep(0.5)
+    py.click(715,500)
+    flag = False
+
+if(color[0]==8):
+    print("New UI")
+    start = [570,540]
+    py.click(start[0],start[1])
+    time.sleep(0.5)
+    py.click(start[0],start[1]+55)
+    print("Selected Option Out of Office")
+    time.sleep(0.5)
+    py.click(start[0]+180,start[1]-30)
+    flag = False
+
+if(flag):
+    print("Check if there is any change in the UI")
+
 print("Clicked on Punched In/Out")
 time.sleep(10)
